e16742702eaec8cd22f355be569e073c7bb939c2
kristofferkvist <nmd499@alumni.ku.dk>
2023-04-12 07:53:51 +0200

    performace tests

--- test_parallel_lowpass.py
+++ test_parallel_lowpass.py
@@ -2,6 +2,7 @@
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+import time
 #My Classes
 from parallel_lowpass import Parallel_lowpass
 from mpi4py import MPI
@@ -13,9 +14,10 @@ rank = world_comm.Get_rank()
 if rank == 0:
     img_file = '/home/kristoffer/Downloads/moon.png'
     image = Image.open(img_file)
+    image = image.resize((1024, 1024))
     image.show()
     img_arr = np.array(image.convert('L'), dtype=np.float64)
-    print(np.sum(img_arr))
+    print(img_arr.shape)
     n1 = np.array(img_arr.shape[0], dtype=np.int32)
     n2 = np.array(img_arr.shape[1], dtype=np.int32)
 else:
@@ -27,9 +29,16 @@ world_comm.Bcast([n1, MPI.INT], root = 0)
 world_comm.Bcast([n2, MPI.INT], root = 0)
 
 lowpasser = Parallel_lowpass(world_comm, n1, n2, 10, 1)
-img_new = lowpasser.blur(img_arr)
 
 if rank == 0:
-    print(np.sum(img_new))
+    t1 = time.time()
+img_new = lowpasser.blur(img_arr)
+if rank == 0:
+    t2 = time.time()
+    print(t2-t1)
+
+
+
+if rank == 0:
     image_new = Image.fromarray(img_new)
     image_new.show()
