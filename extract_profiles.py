febdecfff1999463dd007573b7c793291616e50d
kristofferkvist <nmd499@alumni.ku.dk>
2023-06-20 14:48:31 +0200

    extract ion drift and velocity dist

--- extract_profiles.py
+++ extract_profiles.py
@@ -1,15 +1,9 @@
-import numpy as np
-from boutdata import collect
-import netCDF4 as nc
-from datetime import datetime
-import sys
-
 def main(argv):
     #dt_string = datetime.now().strftime("date_%d_%m_%Y_time_%H_%M_%S")
     field_list  = ['t_array', 'n', 'rhos', 'dx', 'nx', 'dz', 'nz', 'oci', 'te', 'ti', 'n0', 'Te0', 'Ti0', 'B0', 'x_lcfs', 'B', 'phi']
     #n_files = int(argv[1])
     #name_list = map(int, argv[1].strip('[]').split(','))
-    name_list = [48, 49]
+    name_list = [0]
     for i in name_list:
         print('#############################NAME' + str(i)+ '#######################')
         #if (len(argv) == 0):
@@ -21,18 +15,19 @@ def main(argv):
             filename = 'profiles/profiles_no_neutrals' + str(i) + '.nc'
             t_ind_min = 100
         elif (argv[0] == 'fluid'):
-            path = '../kristoffer/fluid_neutrals' + str(i)
+            #path = '../kristoffer/fluid_neutrals' + str(i)
+            path = '/marconi_work/FUA37_SOLF/alec/Neut_Conv/003'
             filename = 'profiles/profiles_fluid' + str(i) + '.nc'
-            field_list = field_list + ['Ncold', 'Nwarm', 'Nhot', 'Sn', 'Spe', 'Spi']
+            field_list = field_list + ['Ncold', 'Nwarm', 'Nhot', 'Sn', 'Spe', 'Spi', 'uSi_x', 'uSi_z']
             t_ind_min = 0
         elif (argv[0] == 'kinetic'):
-            #path = '../kristoffer/kinetic_neutrals' + str(i)
-            #filename = 'profiles/profiles_kinetic' + str(i) + '.nc'
+            path = '../work/kinetic_neutrals' + str(i)
+            filename = 'profiles/profiles_kinetic' + str(i) + '.nc'
             field_list = field_list + ['Sn', 'Spe', 'Spi', 'Sux', 'Suz']
-            #t_ind_min = 100
-            path = 'data'
-            filename = 'profile_test.nc'
-            t_ind_min = 0
+            t_ind_min = 100
+            #path = 'data'
+            #filename = 'profile_test.nc'
+            #t_ind_min = 0
 
     #ds = nc.Dataset('data/BOUT.dmp.0.nc')
     #print("NO NEUTRALS:")
@@ -81,10 +76,15 @@ def main(argv):
         ti = np.squeeze(data['ti'])*Ti0
         ti = ti[t_ind_min:, :, :]
         ti_t_x = np.mean(ti, axis = 2)
-        pe = np.multiply(n, te)
+        pe = np.multiply(n, te*1.602e-19)
        	pe_t_x = np.mean(pe, axis = 2)
-        pi = np.multiply(n, ti)
+        pi = np.multiply(n, ti*1.602e-19)
         pi_t_x = np.mean(pi, axis = 2)
+        _, gradPix, gradPiy = np.gradient(pi)
+        gradPix_t_x = np.mean(gradPix/(dx*rhos), axis = 2)
+        gradPiy_t_x = np.mean(gradPiy/(dy*rhos), axis = 2)
+        print("grad PIx mean = " + str(np.mean(gradPix_t_x)))
+        print("grad PIy	mean = " + str(np.mean(gradPiy_t_x)))
         phi = np.squeeze(data['phi'])*Te0
         phi = phi[t_ind_min:, :, :]
         phi_t_x = np.mean(phi, axis=2)
@@ -95,6 +95,10 @@ def main(argv):
         Ey = -Ey/(dy*rhos)
         Ex_t_x = np.mean(Ex, axis = 2)
         Ey_t_x = np.mean(Ey, axis = 2)
+        u0x_t_x = Ey_t_x/B - gradPiy_t_x/(1.602e-19*n_t_x*B)
+        u0y_t_x = -Ex_t_x/B + gradPix_t_x/(1.602e-19*n_t_x*B)
+        print("u0x_max = " + str(np.max(u0x_t_x)))
+        print("u0y_max = " + str(np.max(u0y_t_x)))
         rad_flux_t_x = np.mean(np.multiply(n, Ey), axis = 2)/B
         rad_flux_Ee_t_x = np.mean(np.multiply(pe, Ey), axis = 2)/B
         rad_flux_Ei_t_x = np.mean(np.multiply(pi, Ey), axis = 2)/B
@@ -107,19 +111,23 @@ def main(argv):
             N_warm_t_x = np.squeeze(N_warm_t_x[t_ind_min:, 2:-2])*n0
             N_hot_t_x = np.mean(data['Nhot'], axis = 3)
             N_hot_t_x = np.squeeze(N_hot_t_x[t_ind_min:, 2:-2])*n0
+            uSix_t_x = np.mean(data['uSi_x'], axis = 3)
+            uSix_t_x = np.squeeze(uSix_t_x[t_ind_min:, :])*np.sqrt(Te0*EV/mi)
+            uSiy_t_x = np.mean(data['uSi_z'], axis = 3)
+            uSiy_t_x = np.squeeze(uSiy_t_x[t_ind_min:, :])*np.sqrt(Te0*EV/mi)
 
         #Densities and speed dists Kinetic neutrals
         if ((len(argv) > 0) and (argv[0] == 'kinetic')):
             ds = nc.Dataset(path + '/neutral_diagnostics.nc')
             print(ds.variables.keys())
-            N_atom_t_x = np.mean(ds['n_atom'], axis = 2)
-            N_hot_t_x = np.mean(ds['n_atom_cx'], axis = 2)
-            N_cold_t_x = np.mean(ds['n_molecule'], axis = 2)
+            N_atom_t_x = np.mean(ds['n_atom'], axis = 2)[t_ind_min:, :]
+            N_hot_t_x = np.mean(ds['n_atom_cx'], axis = 2)[t_ind_min:, :]
+            N_cold_t_x = np.mean(ds['n_molecule'], axis = 2)[t_ind_min:, :]
             N_warm_t_x = N_atom_t_x - N_hot_t_x
 
-            speed_dist_atom = np.mean(ds['speed_dist_atom'], axis = 0)
-            speed_dist_atom_cx = np.mean(ds['speed_dist_atom_cx'], axis = 0)
-            speed_dist_mol = np.mean(ds['speed_dist_mol'], axis = 0)
+            speed_dist_atom = np.mean(ds['speed_dist_atom'][t_ind_min:, :, :], axis = 0)
+            speed_dist_atom_cx = np.mean(ds['speed_dist_atom_cx'][t_ind_min:, :, :], axis = 0)
+            speed_dist_mol = np.mean(ds['speed_dist_mol'][t_ind_min:, :, :], axis = 0)
             bins_atom = np.array(ds['bin_edges_atom'])
             bins_mol = np.array(ds['bin_edges_mol'])
 
@@ -136,62 +144,68 @@ def main(argv):
                 Sux_t_x = np.squeeze(Sux_t_x[t_ind_min:, :])*np.sqrt(Te0*EV*mi)*n0*oci
                 Suz_t_x = np.mean(data['Suz'], axis = 3)
                 Suz_t_x = np.squeeze(Suz_t_x[t_ind_min:, :])*np.sqrt(Te0*EV*mi)*n0*oci
+                uSix_t_x = (Suz_t_x-1.67e-27*Sn_t_x*u0y_t_x)/(1.602e-19*(n_t_x + 0.01*np.mean(n_t_x))*B)
+                uSiy_t_x = (-Sux_t_x+1.67e-27*Sn_t_x*u0x_t_x)/(1.602e-19*(n_t_x + 0.01*np.mean(n_t_x))*B)
 
         nc_dat = nc.Dataset(filename, 'w', 'NETCDF4')
         nc_dat.createDimension('x', xs.size)
         nc_dat.createDimension('x_ng', xs.size-4)
         nc_dat.createDimension('t', ts.size)
-        nc_dat.createDimension('v_doms', speed_dist_atom.shape[0])
-        nc_dat.createDimension('n_bins_atom', speed_dist_atom.shape[1])
-        nc_dat.createDimension('n_bins_mol', speed_dist_mol.shape[1])
-        nc_dat.createDimension('bin_edges_atom', bins_atom.size)
-        nc_dat.createDimension('bin_edges_mol', bins_mol.size)
         t_var = nc_dat.createVariable('ts', 'float32', ('t'))
         t_var[:] = ts
         x_var = nc_dat.createVariable('xs', 'float32', ('x'))
         x_var[:] = xs
-        n_var = nc_dat.createVariable('n_t_x', 'float32', ('t', 'x'))
+        n_var = nc_dat.createVariable('n_x_t', 'float32', ('t', 'x'))
         n_var[:, :] = n_t_x
-        te_var = nc_dat.createVariable('te_t_x', 'float32', ('t', 'x'))
+        te_var = nc_dat.createVariable('te_x_t', 'float32', ('t', 'x'))
         te_var[:, :] = te_t_x
-        ti_var = nc_dat.createVariable('ti_t_x', 'float32', ('t', 'x'))
+        ti_var = nc_dat.createVariable('ti_x_t', 'float32', ('t', 'x'))
         ti_var[:, :] = ti_t_x
-        pe_var = nc_dat.createVariable('pe_t_x', 'float32', ('t', 'x'))
+        pe_var = nc_dat.createVariable('pe_x_t', 'float32', ('t', 'x'))
         pe_var[:, :] = pe_t_x
-        pi_var = nc_dat.createVariable('pi_t_x', 'float32', ('t', 'x'))
+        pi_var = nc_dat.createVariable('pi_x_t', 'float32', ('t', 'x'))
         pi_var[:, :] = pi_t_x
-        phi_var = nc_dat.createVariable('phi_t_x', 'float32', ('t', 'x'))
+        phi_var = nc_dat.createVariable('phi_x_t', 'float32', ('t', 'x'))
         phi_var[:, :] = phi_t_x
-        Ex_var = nc_dat.createVariable('Ex_t_x', 'float32', ('t', 'x'))
+        Ex_var = nc_dat.createVariable('Ex_x_t', 'float32', ('t', 'x'))
         Ex_var[:, :] = Ex_t_x
-        Ey_var = nc_dat.createVariable('Ey_t_x', 'float32', ('t', 'x'))
+        Ey_var = nc_dat.createVariable('Ey_x_t', 'float32', ('t', 'x'))
         Ey_var[:, :] = Ey_t_x
         B_var = nc_dat.createVariable('B', 'float32', ('x'))
         B_var[:] = B
-        rad_flux_var = nc_dat.createVariable('rad_flux_t_x', 'float32', ('t', 'x'))
+        rad_flux_var = nc_dat.createVariable('rad_flux_x_t', 'float32', ('t', 'x'))
         rad_flux_var[: , :] = rad_flux_t_x
-        rad_flux_Ee_var = nc_dat.createVariable('rad_flux_Ee_t_x', 'float32', ('t', 'x'))
+        rad_flux_Ee_var = nc_dat.createVariable('rad_flux_Ee_x_t', 'float32', ('t', 'x'))
         rad_flux_Ee_var[: , :] = rad_flux_Ee_t_x
-        rad_flux_Ei_var = nc_dat.createVariable('rad_flux_Ei_t_x', 'float32', ('t', 'x'))
+        rad_flux_Ei_var = nc_dat.createVariable('rad_flux_Ei_x_t', 'float32', ('t', 'x'))
         rad_flux_Ei_var[: , :] = rad_flux_Ei_t_x
         if ((len(argv) > 0) and (argv[0] == 'fluid' or argv[0] == 'kinetic')):
-            Ncold_var = nc_dat.createVariable('Ncold_t_x', 'float32', ('t', 'x_ng'))
+            Ncold_var = nc_dat.createVariable('Ncold_x_t', 'float32', ('t', 'x_ng'))
             Ncold_var[:, :] = N_cold_t_x
-            Nwarm_var = nc_dat.createVariable('Nwarm_t_x', 'float32', ('t', 'x_ng'))
+            Nwarm_var = nc_dat.createVariable('Nwarm_x_t', 'float32', ('t', 'x_ng'))
             Nwarm_var[:, :] = N_warm_t_x
-            Nhot_var = nc_dat.createVariable('Nhot_t_x', 'float32', ('t', 'x_ng'))
+            Nhot_var = nc_dat.createVariable('Nhot_x_t', 'float32', ('t', 'x_ng'))
             Nhot_var[:, :] = N_hot_t_x
-            Sn_var = nc_dat.createVariable('Sn_t_x', 'float32', ('t', 'x'))
+            Sn_var = nc_dat.createVariable('Sn_x_t', 'float32', ('t', 'x'))
             Sn_var[:, :] = Sn_t_x
-            Spe_var = nc_dat.createVariable('Spe_t_x', 'float32', ('t', 'x'))
+            Spe_var = nc_dat.createVariable('Spe_x_t', 'float32', ('t', 'x'))
             Spe_var[:, :] = Spe_t_x
-            Spi_var = nc_dat.createVariable('Spi_t_x', 'float32', ('t', 'x'))
+            Spi_var = nc_dat.createVariable('Spi_x_t', 'float32', ('t', 'x'))
             Spi_var[:, :] = Spi_t_x
+            uSix_var = nc_dat.createVariable('uSix_x_t', 'float32', ('t', 'x'))
+            uSix_var[:, :] = uSix_t_x
+            uSiy_var = nc_dat.createVariable('uSiy_x_t', 'float32', ('t', 'x'))
+            uSiy_var[:, :] = uSiy_t_x
         if ((len(argv) > 0) and (argv[0] == 'kinetic')):
-            Sux_var = nc_dat.createVariable('Sux_t_x', 'float32', ('t', 'x'))
+            Sux_var = nc_dat.createVariable('Sux_x_t', 'float32', ('t', 'x'))
             Sux_var[:, :] = Sux_t_x
-            Suz_var = nc_dat.createVariable('Suz_t_x', 'float32', ('t', 'x'))
+            Suz_var = nc_dat.createVariable('Suz_x_t', 'float32', ('t', 'x'))
             Suz_var[:, :] = Suz_t_x
+            nc_dat.createDimension('v_doms', speed_dist_atom.shape[0])
+            nc_dat.createDimension('n_bins_atom', speed_dist_atom.shape[1])
+            nc_dat.createDimension('n_bins_mol', speed_dist_mol.shape[1])
+            nc_dat.createDimension('bin_edges_atom', bins_atom.size)
+            nc_dat.createDimension('bin_edges_mol', bins_mol.size)
             speed_dist_atom_var = nc_dat.createVariable('speed_dist_atom', 'float32', ('v_doms', 'n_bins_atom'))
             speed_dist_atom_var[:, :] = speed_dist_atom
             speed_dist_atom_cx_var = nc_dat.createVariable('speed_dist_atom_cx', 'float32', ('v_doms', 'n_bins_atom'))
